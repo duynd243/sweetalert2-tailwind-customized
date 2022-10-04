c8d7c7d231436e8d27f7cdce475c7e707f5a3b0d
duyndse <duyndse@gmail.com>
2022-10-04 23:47:12 +0700

    Init commit

---
+++ src/script.js
@@ -0,0 +1,9 @@
+function showAlert() {
+    Swal.fire({
+        title: 'Hello World',
+        text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+        icon: 'success',
+        showCancelButton: true,
+        confirmButtonText: 'Confirm'
+    })
+}
